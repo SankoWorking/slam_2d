d1568f7e16013dfd5331ce82904993269c8aed80
Sanko <sanko_working@163.com>
2026-03-20 19:08:55 +0800

    在bringup包中集了启动slam节点及参数配置文件

---
+++ src/robot_bringup/launch/slam.launch.py
@@ -0,0 +1,21 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_bringup = get_package_share_directory('robot_bringup')
+    slam_params_file = os.path.join(pkg_bringup, 'config', 'slam_params.yaml')
+
+    return LaunchDescription([
+        Node(
+            package='slam_toolbox',
+            executable='async_slam_toolbox_node',
+            name='slam_toolbox',
+            output='screen',
+            parameters=[
+                slam_params_file,
+                {'use_sim_time': False}
+            ]
+        )
+    ])
