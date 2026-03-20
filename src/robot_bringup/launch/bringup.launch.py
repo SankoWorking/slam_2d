8fb3317572b2e7e96896dc89f7915047735085e0
Sanko <sanko_working@163.com>
2026-03-20 18:55:15 +0800

    搭建了robot_bringup包的基本框架

---
+++ src/robot_bringup/launch/bringup.launch.py
@@ -0,0 +1,18 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+def generate_launch_description():
+    pkg_serial = get_package_share_directory('robot_serial')
+    pkg_imu    = get_package_share_directory('robot_imu')
+    pkg_lidar  = get_package_share_directory('robot_lidar')
+    pkg_odom   = get_package_share_directory('robot_odometry')
+
+    return LaunchDescription([
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_serial, 'launch', 'serial.launch.py'))),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_imu, 'launch', 'imu_setup.launch.py'))),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_lidar, 'launch', 'lidar_all.launch.py'))),
+        IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(pkg_odom, 'launch', 'odom.launch.py'))),
+    ])
